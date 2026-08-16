4cd68e812cf410ff478c3aa4eee070164743965c
Abhishekkumar503 <ak204479@gmail.com>
2026-08-16 23:42:02 +0530

    Playwright core concept

--- tests/UIBasictest.spec.js
+++ tests/UIBasictest.spec.js
@@ -1,4 +1,4 @@
-const {test} = require('@playwright/test'); // import playwright
+const {test, expect} = require('@playwright/test'); // import playwright
 
 
 // testcase defination format with browser
@@ -13,4 +13,7 @@ test('Browser Context Playwright test',async ({browser}) =>
 test('Page fixture Playwright test',async ({page}) =>
 {
     await page.goto("https://rahulshettyacademy.com/practice")
+    console.log(await page.title())
+    // Assertions
+    await expect(page).toHaveTitle("QA Automation Practice Sites | Playwright, Selenium & API Testing")
 });
